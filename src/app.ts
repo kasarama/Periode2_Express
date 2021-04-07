b5bdb20087dca5e1e85615336a21aafb683a969b
kasarama <magdalena.wawrzak@gmail.com>
2021-04-08 00:19:04 +0200

    first middleware

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,32 @@
 import express from "express";
-
 import path from "path";
-
 import dotenv from "dotenv";
-
-import facade from ".//facades/DummyDB-Facade";
-
 import frienRouts from "./routes/frinedsRouts";
+const debug = require("DEBUG")("app");
 dotenv.config();
 const app = express();
+/*
+app.use((req, res, next) => {
+  //logging to console makes a mess, better is logging
+  console.log(
+    `${new Date().toLocaleTimeString()}\n${req.method}\n${req.url}\nip: ${
+      req.ip
+    }\nremoteAddres${req.connection.remoteAddress}`
+  );
 
+  console.log("request made");
+  debug(
+    `${new Date().toLocaleTimeString()}\n${req.method}\n${req.url}\nip: ${
+      req.ip
+    }\nremoteAddres${req.connection.remoteAddress}`
+  );
 
- 
-
-
+  next();
+});
+*/
 //app.use('/static', express.static('public'))
 app.use(express.static(path.join(process.cwd(), "public"))); //cwd = current working directory
-app.use(express.json())
+app.use(express.json());
 app.get("/demo", (req, res) => {
   let a = 123;
   // a='dss' gives fail that typescripts wants it to be of the first instantiated
--- src/bin/www.ts
+++ src/bin/www.ts
@@ -5,11 +5,11 @@ not working:
 import d from "debug";
 const debug = d("www");
 */
-const debug = require('DEBUG')('www')
+const debug = require("DEBUG")("www");
 
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log(`Litening on port ${PORT}`);
-  debug(`Server started.Litening on port ${PORT}`)
+  debug(`Server started.Litening on port ${PORT}`);
 });
